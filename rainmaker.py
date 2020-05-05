500c611e44c363e153b4358e94c65c5b95fce55b
cynic_custodian <cynic_custodian@mailbox.org>
2020-05-05 12:40:06 +0100

    Created a working definition for turning on the pump and turning it off with button 'B'. Stopped at creating the 'on' function for button 'A'

--- rainmaker.py
+++ rainmaker.py
@@ -1,16 +1,62 @@
 import logging
-import datetime
-from gpiozero import Button, LED
+from datetime import datetime
+#from gpiozero import Button, LED
+import RPi.GPIO as GPIO
 
 #logging
 logging.basicConfig(filename='rainmaker.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s: %(message)s')
 
 #GPIO
-A = Button(22)
-B = Button(23)
-C = Button(24)
-D = Button(25)
-RedButton = Button (26)
-GreenButton = Button (27)
-RedLed = LED(16)
-GreenLed = LED(17)
+GPIO.setmode(GPIO.BCM)
+A = 22
+GPIO.setup(A, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+B = 23
+GPIO.setup(B, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+C = 24
+GPIO.setup(C, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+D = 25
+GPIO.setup(D, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+RedButton = 26
+GPIO.setup(RedButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GreenButton = 27
+GPIO.setup(GreenButton, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+RedLed = 16
+GreenLed = 17
+PumpRelais = 0
+GPIO.setup(RedLed,GPIO.OUT)
+GPIO.setup(GreenLed,GPIO.OUT)
+GPIO.setup(PumpRelais,GPIO.OUT)
+
+''' the magic '''
+#Turns pump on
+def pump_on():
+	TimeOn = datetime.now()
+	logging.info('Pump turned on at %s', TimeOn)
+	while True:
+		GPIO.output(PumpRelais,GPIO.HIGH)
+		print(TimeOn)
+		if GPIO.input(B):
+			logging.info('Pump turned off')
+			GPIO.output(PumpRelais,GPIO.LOW)
+			break
+
+pump_on()
+
+#		else:
+#			break
+#def input():
+#	if GPIO.input(A)
+#	pump_on()
+
+#def input():
+#
+#if __name__ == "__main__":
+#	try:
+#		pump_on()
+#		main_loop()
+#	except RuntimeError as error:
+#		print(error.args[0])
+#	except KeyboardInterrupt:
+#		print("\nExiting application\n")
+#		# exit the applications
+#		GPIO.cleanup()
